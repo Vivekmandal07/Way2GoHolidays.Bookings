578425813b54c7558b6986a5675b24e2b822ba3a
Vivek Mandal <121191537+Vivekmandal07@users.noreply.github.com>
2026-03-10 13:47:34 +0000

    malaysia image 2nd time upload

--- assets/images.ts
+++ assets/images.ts
@@ -18,7 +18,7 @@ export const PICS = {
   dubai: 'https://images.unsplash.com/photo-1512453979798-5ea266f8880c?auto=format&fit=crop&w=600&q=80',
   bali: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=600&q=80',
   singapore: 'https://images.unsplash.com/photo-1525625239514-75b4b17b9979?auto=format&fit=crop&w=600&q=80',
-  malaysia: 'https://www.istockphoto.com/photo/petronas-towers-gm466842820-59952574?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_bottom&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fmalaysia-image&utm_term=malaysia+image%3A%3Aaffiliate-signature-content%3Acontrol%3A8574670a-1f74-4c40-b7cc-76fc61fa403a',
+  malaysia: 'https://plus.unsplash.com/premium_photo-1700955569575-439bed2d50f2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fG1hbGF5c2lhJTIwaW1hZ2V8ZW58MHx8MHx8fDA%3D',
   vietnam: 'https://images.unsplash.com/photo-1528127269322-539801943592?auto=format&fit=crop&w=600&q=80',
   thailand: 'https://images.unsplash.com/photo-1552465011-b4e21bf6e79a?auto=format&fit=crop&w=800&q=80', // Iconic Thailand longtail boat in turquoise water
   europe: 'https://images.unsplash.com/photo-1499856871958-5b9627545d1a?auto=format&fit=crop&w=600&q=80',
