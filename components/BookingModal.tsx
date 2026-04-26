8e5607d408cdddbb4fbe145ef1fc4f0176d3c321
Vivek Mandal <121191537+Vivekmandal07@users.noreply.github.com>
2026-04-26 11:13:48 +0000

    age

--- components/BookingModal.tsx
+++ components/BookingModal.tsx
@@ -224,7 +224,7 @@ const BookingModal: React.FC<BookingModalProps> = ({ onClose }) => {
               <div className="md:col-span-2 bg-blue-50/10 p-3 rounded-2xl border border-blue-100 flex flex-wrap gap-3 animate-in fade-in duration-300 shadow-inner">
                 {formData.childAges.map((age, i) => (
                   <div key={i} className="flex flex-col space-y-1">
-                    <span className="text-[8px] font-bold text-blue-500 uppercase ml-1 tracking-wider">Child {i+1} yrs</span>
+                    <span className="text-[8px] font-bold text-blue-500 uppercase ml-1 tracking-wider">Child {i+1} Age</span>
                     <input 
                       required type="number" min="0" max="17" 
                       className="w-14 px-2 py-2 border border-slate-200 rounded-lg text-center font-bold text-xs text-black focus:border-blue-500 outline-none bg-white" 
