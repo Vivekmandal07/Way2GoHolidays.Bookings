79e7161b95d19993f8eb5afb3c4f865103d82ef8
Vivek Mandal <121191537+Vivekmandal07@users.noreply.github.com>
2026-05-17 06:22:13 +0000

    email phone enterchng

--- components/BookingModal.tsx
+++ components/BookingModal.tsx
@@ -142,7 +142,7 @@ const BookingModal: React.FC<BookingModalProps> = ({ onClose }) => {
               />
             </div>
 
-            <div className="md:col-span-2 space-y-1">
+            <div className="space-y-1">
               <label className="text-[10px] font-bold text-slate-400 uppercase tracking-widest ml-1">WhatsApp Number</label>
               <div className="flex items-stretch border-2 border-slate-200 rounded-xl focus-within:border-blue-500 focus-within:ring-4 focus-within:ring-blue-50/20 transition-all overflow-hidden bg-slate-50/50 shadow-sm">
                 <select 
@@ -165,7 +165,7 @@ const BookingModal: React.FC<BookingModalProps> = ({ onClose }) => {
               </div>
             </div>
 
-            <div className="space-y-1">
+            <div className="md:col-span-2 space-y-1">
               <label className="text-[10px] font-bold text-slate-400 uppercase tracking-widest ml-1">Email Address</label>
               <input 
                 required type="email" 
